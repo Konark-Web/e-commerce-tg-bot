1a3a7066e68c3e58d1456c61b0907a79c298f205
Vladyslav Spesyvets <vladyslav.spesyvets@alpha-bots.com>
2022-01-25 19:33:11 +0200

    Added new django project Added new django app Connected postgres Added new model (BotConfig)

---
+++ app/tbot/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import BotConfig
+
+admin.site.register(BotConfig)
---
+++ app/tbot/models.py
@@ -0,0 +1,39 @@
+import telebot
+
+from django.db import models
+
+
+class BotConfig(models.Model):
+    title = models.CharField(verbose_name='Назва бота', max_length=80,
+                             blank=True, default='')
+    token = models.CharField(verbose_name='Токен бота', max_length=100,
+                             primary_key=True)
+    server_url = models.CharField(verbose_name='Webhook Url', max_length=200,
+                                  blank=True, default='')
+    nova_poshta_api = models.CharField(verbose_name='Nova Poshta API', max_length=200,
+                                       blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def set_hook(self):
+        bot = telebot.TeleBot(self.token)
+        webhook_url = str(self.server_url) + '/get_hook/'
+        bot.set_webhook(webhook_url)
+
+    def set_active_config(self):
+        if self.is_active:
+            other_active_configs = BotConfig.objects.filter(is_active=True)
+            for config in other_active_configs:
+                if config.pk != self.pk:
+                    config.is_active = False
+                    config.save()
+
+    def save(self, *args, **kwargs):
+        self.set_hook()
+        self.set_active_config()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+
