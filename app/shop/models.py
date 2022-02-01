b89f21421e197f4cf8d74172a902bdedcec06c8d
Vladyslav Spesyvets <vladyslav.spesyvets@alpha-bots.com>
2022-02-01 12:31:45 +0200

    Refactored customer model for correctly view in admin

--- app/shop/models.py
+++ app/shop/models.py
@@ -5,15 +5,18 @@ class Customer(models.Model):
     telegram_id = models.CharField('Telegram ID', max_length=100, primary_key=True)
     username = models.CharField('Username', max_length=200,
                                 blank=True, null=True, default=None)
-    customer_name = models.CharField('ФІО користувача', max_length=255, default='')
+    customer_name = models.CharField('ФІО користувача', max_length=255, blank=True, default='')
     phone_number = models.CharField('Номер телефону', max_length=64, blank=True, default='')
     city = models.CharField('Місто', max_length=200, blank=True, default='')
     date_joined = models.DateTimeField('Дата реєстрації', auto_now_add=True)
-    state = models.CharField(max_length=200, blank=True, null=True, default=None)
+    state = models.CharField(max_length=200, blank=True, null=True, default=None, editable=False)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
-        return f'{self.telegram_id} {self.username}'
+        if self.customer_name:
+            return f'[{self.telegram_id}] {self.customer_name}'
+        else:
+            return f'[{self.telegram_id}] {self.username}'
 
     def get_state(self):
         return self.state
